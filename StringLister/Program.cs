55eec9a494a114f184c54450f378df913fefd276
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-02-08 22:57:57 -0600

    Initial commit

---
+++ StringLister/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringLister
+{
+	class Program
+	{
+		static List<string> list = new List<string>();
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				
+				var inputString = Console.ReadLine();
+				if (inputString != "") 
+				list.Add(inputString);
+
+				var result = String.Join("\n", list.ToArray());
+				Console.WriteLine(result);
+
+			}
+			
+		}
+	}
+}
