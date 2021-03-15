87063730e6209a1c52efd110dabf108747d47d6c
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-03-14 21:15:03 -0500

    Added program that lists unique chars in string and how many times they occur.

---
+++ CharOccurance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CharOccurance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a string to process:");
+			var input_string = Console.ReadLine().ToCharArray();
+
+			var unique_string = new String(input_string.Distinct().ToArray());
+
+			Console.WriteLine();
+
+			foreach (char c in unique_string)
+			{
+				int occurances = input_string.Count(f => f == c);
+				Console.WriteLine(c + ": " + occurances);
+			}
+			Console.ReadKey();
+		}
+	}
+}
