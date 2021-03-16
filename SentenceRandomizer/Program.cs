40ed6843357b5c9b7b881b68a1572320138c2ec1
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-03-16 00:00:13 -0500

    Added a program to shuffle an inputted string

---
+++ SentenceRandomizer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SentenceRandomizer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//input of a string separated by " " to be randomized and returned
+			
+			Console.WriteLine("Enter your input string: ");
+			var input_string = Console.ReadLine();
+
+			string[] words = input_string.Split(' ');
+
+			Random r = new Random();
+			string[] shuffled_array = words.OrderBy(x => r.Next()).ToArray();
+
+			var result = String.Join(" ", shuffled_array);
+
+			Console.Write(result);
+			Console.ReadKey();
+		}
+	}
+}
