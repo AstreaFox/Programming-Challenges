94a0c6e8ebad9834866d81f6b6d9da1d347819aa
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-03-19 20:42:16 -0500

    Added string equation calculator

---
+++ SImpleStringCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace SImpleStringCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.Write("Calculation: ");
+				var input = Console.ReadLine();
+				DataTable dt = new DataTable();
+				Console.WriteLine("Answer: " + dt.Compute(input, ""));
+				Console.WriteLine();
+			}
+		}
+	}
+}
