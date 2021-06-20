0f4b30645a8ec42aaa7f4642d0b0e785ad5a18ed
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-06-20 14:50:39 -0500

    Initial code commit.

---
+++ IntenseStringSpammer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntenseStringSpammer
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.Write("Enter string: ");
+			var spam_string = Console.ReadLine();
+
+			Console.Write("Enter the limit: ");
+			var limit = Convert.ToInt32(Console.ReadLine());
+
+			var nums = Enumerable.Range(1, limit);
+
+			var strs = nums.Select(i =>
+			{
+				var rep_arr = Enumerable.Repeat(spam_string, i);
+				var rep_str = "*" + string.Join(" ", rep_arr) + "*";
+				return rep_str;
+			});
+
+			var result = string.Join("\n", strs);
+
+			Console.WriteLine(result);
+			Clipboard.SetText(result);
+
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ PulsatingASCII/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PulsatingASCII
+{
+	static class Program
+	{
+		static void PrintArray(string[] lines) =>
+			Console.WriteLine(string.Join("\n", lines));
+
+		static string LineOfDots(int innerWidth, int outerWidth) =>
+			new string(' ', (outerWidth - innerWidth) / 2) +
+			new string('.', innerWidth) +
+			new string(' ', (outerWidth - innerWidth) / 2);
+
+		static void DrawDiamond(int size)
+		{
+			Console.Clear();
+
+			var center = size / 2;
+			var lines = new string[size];
+
+			lines[center] = LineOfDots(size, size);
+			for (var i = 1; i <= size / 2; i++)
+			{
+				lines[center - i] = LineOfDots(size - i * 2, size);
+				lines[center + i] = LineOfDots(size - i * 2, size);
+			}
+
+			PrintArray(lines);
+
+			Thread.Sleep(50);
+		}
+
+		static void Main(string[] args)
+		{
+			var size = 11;
+
+			while (true)
+			{
+				for (var i = 1; i <= size; i += 2) DrawDiamond(i);
+				for (var i = size; i >= 1; i -= 2) DrawDiamond(i);
+			}
+		}
+	}
+}
