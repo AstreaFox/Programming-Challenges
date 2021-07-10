3e74e1e83d7bc3a6458a055a6b270680f58941f2
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-07-09 21:04:35 -0500

    Commited web application to demonstrate the fundamentals of a full-stack JS REST C# application

---
+++ JavaScript/JS REST API C#/JS_REST_API/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SimpleHttp;
+
+namespace JS_REST_API
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var messages = new List<string>();
+
+			var server = new HttpServer(80);
+
+			server.AddExactRoute("GET", "/", (request, response) =>
+			{
+				response.WriteBodyText("OK");
+			});
+
+			server.AddExactRoute("PUT", "/messages", (request, response) =>
+			{
+				messages.Add(request.ReadBodyText());
+			});
+
+			server.AddExactRoute("GET", "/messages", (request, response) =>
+			{
+				response.WriteBodyJson(messages);
+			});
+
+			server.AddStaticDirectory("/web", @"C:\Users\Fox\Documents\Visual Studio 2019\Projects\JavaScript\JS REST API C#");
+
+			server.Start();
+
+			Thread.Sleep(Timeout.Infinite);
+		}
+	}
+}
