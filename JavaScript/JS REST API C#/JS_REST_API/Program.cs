22f1c180aca61da4de0561c1d05aa33d9fc095fc
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-07-13 22:31:35 -0500

    Version 3 - Added support for preventing messages without a username, added character length limit to messages, added saving/restoring messages from .txt file and added root endpoint redirect (no need to type full URL).

--- JavaScript/JS REST API C#/JS_REST_API/Program.cs
+++ JavaScript/JS REST API C#/JS_REST_API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,18 +12,30 @@ namespace JS_REST_API
 	{
 		static void Main(string[] args)
 		{
+			var messageFile = Path.GetFullPath("messages.txt");
+			if (!File.Exists(messageFile)) File.Create(messageFile).Close();
+
 			var messages = new List<string>();
+			messages.AddRange(File.ReadAllLines(messageFile));
 
 			var server = new HttpServer(80);
 
 			server.AddExactRoute("GET", "/", (request, response) =>
 			{
-				response.WriteBodyText("OK");
+				response.RedirectLocation = "/web/JSREST.html";
 			});
 
 			server.AddExactRoute("PUT", "/messages", (request, response) =>
 			{
-				messages.Add($"[{request.ClientIP}] {request.ReadBodyText()}");
+				var text = $"[{request.ClientIP}] {request.ReadBodyText()}";
+
+				if (text.Length > 500)
+					text = "** Messages greater than 500 characters are not allowed. **";
+
+				// prevent clashing of user requests (messages) sent at the same time
+				lock (messageFile) File.AppendAllText(messageFile, text + "\n");
+
+				messages.Add(text);
 			});
 
 			server.AddExactRoute("GET", "/messages", (request, response) =>
