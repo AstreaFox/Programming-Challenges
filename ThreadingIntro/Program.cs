0cd8a16db0a20ffd1b75a426e216d1772a65852f
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-08-01 22:28:53 -0500

    Added a basic program that demonstrates the usage of threads (multi-threading)

---
+++ ThreadingIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					Console.WriteLine("Thread 1!");
+					Thread.Sleep(1000);
+				}
+			});
+
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					Console.WriteLine("Thread 2!");
+					Thread.Sleep(1000);
+				}
+			});
+
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					Console.WriteLine("Thread 3!");
+					Thread.Sleep(1000);
+				}
+			});
+
+			Thread.Sleep(Timeout.Infinite);
+		}
+	}
+}
