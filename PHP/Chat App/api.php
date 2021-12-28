2dcf8ee3e5c34e011774482038b62f3339b22ba6
Astrea Sable Fox <astrea.sable.fox@gmail.com>
2021-12-27 19:48:43 -0600

    Added a PHP variant of Chat App

---
+++ PHP/Chat App/api.php
@@ -0,0 +1,31 @@
+<?php
+
+	$method = $_SERVER['REQUEST_METHOD'];
+	$body = file_get_contents('php://input');
+
+	$db = pg_connect("host=10.0.2.27 user=postgres password= dbname=challenges");
+
+	if ($method === "GET")
+	{
+		$messages = pg_fetch_all(pg_query($db, "SELECT * FROM chat_app.messages"));
+		$formattedMessages = array_map(function ($message) 
+			{ return "[{$message["timestamp"]}] <{$message["user_name"]}> {$message["message"]}"; }, 
+			$messages);
+
+		echo json_encode($formattedMessages);
+	}
+
+	if ($method === "PUT")
+	{
+		$message = json_decode($body, true);
+
+		$datetime = new DateTime("now", new DateTimeZone('America/Chicago'));
+
+		pg_prepare($db, "",
+			"INSERT INTO chat_app.messages (user_name, message, timestamp) " .
+			"VALUES ($1, $2, $3)");
+		pg_execute($db, "",
+			[ $message["user"], $message["message"], $datetime->format(DateTime::ATOM) ]);
+	}
+
+?>
